9699ef5a8934062f14f4f0fbf4815f7197c07cd0
Natalia Szpurko-Pawłuszko <natalia.szpurko@gmail.com>
2020-03-26 16:28:42 +0100

    recommendations

--- alergyAlert/alergyAlert/tests.py
+++ alergyAlert/alergyAlert/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import pytest
 
 # Create your tests here.
+from django.urls import reverse
+
+from alergyAlert.models import *
+from django.contrib.auth.models import User
+
+class MealModelTest(TestCase):
+    def setUp(self):
+        User.objects.create()
+        Meal.objects.create(name='pizza', description='simple&tasty', user_id=User.objects.get(id=1))
+
+    def test_meal_setup(self):
+        pizza= Meal.objects.get(name="pizza")
+        self.assertEqual(pizza.description, "simple&tasty")
+
+
+class LoginMealsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('user1', 'user1@user', 'classified')
+        self.user = User.objects.create_user('user2', 'user2@user', 'classified2')
+        self.meal = Meal.objects.create(name='scrumble eggs', description='tasty but fat rich', user_id=User.objects.get(id=2))
+
+    def test_login_and_meals_view(self):
+        self.client.login(username='user1', password='classified')
+        response = self.client.get(reverse('meals'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'alergyAlert/meals.html')
+
---
+++ readme.md
@@ -0,0 +1,30 @@
+
+#  alergyAlert
+app in Django for alergy sufferers to collect data on daily menu and make observations of food alergies symptoms.
+
+## Installation (on Ubuntu)
+
+- django == 2.2.9,
+- django-cors-headers == 3.2.1,
+- psycopg2 == 2.7.4,
+- pgAdmin4
+- [API nutritionix] (https://developer.nutritionix.com/docs/v2)
+
+## Usage
+Add your meals and observe alergies' symptoms. Reccomendations system and nutritionix API counting number of kcal in a serving of a given meal included.
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## History
+This app was made for private purpose.
+
+## Credits
+I've learnt solutions used in the app from CodersLab IT course as well
+as Stack Overflow :-)
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
